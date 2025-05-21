ed98f5ef0a94b0dfbd0a9b8d145e1a21a23974e6
kartikMpatel <kartikpatel7892@gmail.com>
2025-05-21 10:23:30 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+const express = require('express') // Common JS
+// import express from "express" -> new Pattern // Module JS 
+const app = express()
+const port = process.env.PORT || 8080
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/hello', (req, res) => {
+  res.send('Hello Kartik!')
+})
+
+app.get('/login',(req,res)=>{
+    res.send("<h1>You Are Not Logged In Yet..</h1>")
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
